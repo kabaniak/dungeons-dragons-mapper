8778e3c18e248d5be9936979e25663e95e8b150e
Glitch (dungeons-dragons-map) <none>
2020-09-11 03:48:07 +0000

    🌁☂️ Updated with Glitch

--- app/components/ImageUpload.jsx
+++ app/components/ImageUpload.jsx
@@ -2,15 +2,51 @@ const React = require("react");
 
 /* styling */
 const imgStyle = {
-  height: '100%',
-  width: '100%',
+  position: 'absolute',
+  top: '170px',
+  left: '25px',
+  height: '600px',
   border: 'hidden'
 }
 const selectorStyle = {
   border: 'hidden'
 }
+const gridStyle = {
+  position: 'absolute',
+  top: '170px',
+  left: '25px',
+  zIndex: '2',
+  display: 'flex',
+  flexDirection: 'column'
+}
+const gridRowStyle = {
+  border: '1px solid black',
+  minWidth: '50px',
+  minHeight: '50px',
+  display: 'flex',
+  flexDirection: 'row'
+}
+const gridCellStyle = {
+  border: '1px solid black',
+  minWidth: '50px',
+  minHeight: '50px',
+  display: 'inline-block'
+}
 
-/* the main page for the index route of this app */
+/* function to add the appropriate amount of rows */
+function addRow(x){
+  let num = [];
+  for(let i=0; i<x; i++){num.push(i)}
+  return (
+    <div id="gridRow" style={gridRowStyle}>
+      {num.map(i => addCell())}
+    </div>);
+}
+function addCell(){
+  return (<div id="gridItem" style={gridCellStyle} ></div>);
+}
+
+/* rendering image upload function*/
 const ImageUpload = function() {
   window.addEventListener("load", function() {
     document
@@ -22,12 +58,19 @@ const ImageUpload = function() {
         }
       });
   });
+  
+  let num = [];
+  for(let i=0; i<10; i++){num.push(i)}
 
   return (
     <div>
       <input type="file" id="fileSelector" style={selectorStyle}/>
       
       <img id="myImg" alt="" style={imgStyle}/>
+      
+      <div id="theGrid" style={gridStyle}>
+        {num.map(i => addRow(10))}
+      </div>
     </div>
   );
 };
